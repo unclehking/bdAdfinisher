81f9523b455a0c677eb76998d6b3c3e490ceaeed
unclehking <513935463@qq.com>
2016-05-06 10:01:44 +0800

    baiduadfinisher

---
+++ background.js
@@ -0,0 +1,21 @@
+﻿/*HKing 2016-03-10*/
+chrome.browserAction.onClicked.addListener(function(tab) {
+
+	chrome.tabs.getAllInWindow(null, function(tabs) {
+		var baidu ;
+		for (var i = 0; i < tabs.length; i++) {
+			var t = tabs[i].url;
+			if (t.match('www.baidu.com')) {
+				var tab = tabs[i];
+				baidu = tabs[i];
+			}
+		}
+		if(baidu){
+			chrome.tabs.update(baidu.id, {selected:true});
+		}else{
+			chrome.tabs.create({"url":"http://www.baidu.com/", "selected":true});
+		};
+	});
+
+});
+alert(1);
