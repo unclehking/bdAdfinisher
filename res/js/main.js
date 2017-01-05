2f433f67d69d6d407c4afea27ead31fe1cbc4f61
unclehking <513935463@qq.com>
2017-01-05 12:29:32 +0800

    修正移动版首页导航遮盖住搜索输入框的bug

--- res/js/main.js
+++ res/js/main.js
@@ -1,29 +1,12 @@
-//设置歌词背景图片
-setLrcBg = (src,fe) =>{
-	let t = fe.querySelector(".link-wrapper");
-	if(!t.querySelector("img")){
-		t.innerHTML = `<img src="${src}" />`;
-	}else{
-		t.querySelector("img").src = src;
-	}
-}
 
-//双击显示歌词区域进入/退出全屏“ktv”歌词模式
+//移动版 输入搜索关键词时隐藏导航
 document.addEventListener('DOMContentLoaded', ()=>{
-	let lrcTarget = document.querySelector("#lrcCol");
-	if(!lrcTarget){
-		return;
-	}
-	let tImg = lrcTarget.querySelector("img");
-	tImg.addEventListener('load', ()=>{
-		setLrcBg(tImg.src,lrcTarget);
-	});
-	lrcTarget.addEventListener('dblclick', ()=>{
-		if(document.mozFullscreenElement != null){
-			document.mozkitCancelFullScreen();
-		}else {
-			lrcTarget.mozkitRequestFullScreen();
-		};
-	});
-	setLrcBg(tImg.src,lrcTarget);
+	var bNav = document.querySelector("#navs");
+	var kw = document.querySelector("#index-kw");
+	kw.onfocus = function(){
+		bNav.style.display = "none";
+	};
+	kw.onblur = function(){
+		bNav.style.display = "block";
+	};
 });
