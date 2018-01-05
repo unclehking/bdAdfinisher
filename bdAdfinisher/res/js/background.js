05a748a74ed9a9261fe970849a95794fd02c9992
unclehking <513935463@qq.com>
2018-01-05 10:36:06 +0800

    new style

--- bdAdfinisher/res/js/background.js
+++ bdAdfinisher/res/js/background.js
@@ -1,14 +1,1 @@
-﻿/*HKing 2016-03-10*/
-const  url="www.baidu.com";
-chrome.browserAction.onClicked.addListener((tab) =>{
-	chrome.tabs.getAllInWindow(null, fuckBat = tabs =>{
-		for(var t of tabs){
-			if (t.url.match(url)) {
-				fuckBat.bTab = true;
-				chrome.tabs.update(t.id, {selected:true});
-				break;
-			}
-		}
-		!fuckBat.bTab && chrome.tabs.create({"url":`https://${url}`, "selected":true});
-	});
-});
+﻿
---
+++ bdAdfinisher/res/js/popup.js
@@ -0,0 +1,25 @@
+
+const url="www.baidu.com";
+var app = angular.module("App",[]);
+app.controller("popup",function($scope,$http,$interval){
+	$scope.goWechat = function(){
+		chrome.tabs.getAllInWindow(null, fuckBat = tabs =>{
+			for(var t of tabs){
+				if (t.url.match(url)) {
+					fuckBat.bTab = true;
+					chrome.tabs.update(t.id, {selected:true});
+					break;
+				}
+			}
+			!fuckBat.bTab && chrome.tabs.create({"url":`https://${url}`, "selected":true});
+		});
+	}
+	//打赏
+	$scope.pay = {
+		index:0
+	}
+});
+
+
+
+
